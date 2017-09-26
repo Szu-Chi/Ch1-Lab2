b631e2314d44228fdf78c8872e718cd7817769ca
Szu-Chi <r310102930719@gmail.com>
2017-09-26 15:07:42 +0800

    Complete Ch1-Lab2

---
+++ source/User_defined.c
@@ -0,0 +1,7 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int User_defined_print() 
+{
+	printf("Welcome to C !\n");
+};
---
+++ source/main.c
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"User_defined.h"
+int main(void) {
+	User_defined_print();
+	system("pause");
+	return 0;
+}
